648ce936bf006d1c38e3e0764e3a8d7e7e4e53e8
Mukulrai2000 <smukulsharma308@gmail.com>
2023-10-07 20:48:42 +0530

    test frontend

--- client/src/components/TodoTabs.jsx
+++ client/src/components/TodoTabs.jsx
@@ -3,7 +3,11 @@ import React,{Fragment} from 'react'
 const TodoTabs = () => {
   return (
     <Fragment>
+        {/* <h1>For Preview</h1> */}
         <h1>For Preview</h1>
+        <h1>For Preview</h1>
+        <h1>For Preview</h1>
+        {/* <h1>For Preview</h1> */}
     </Fragment>
   )
 }
